55533f9e500fe7a2c98f8ef37c78022578663e99
ahmadraza <ahmadraza24415@gmail.com>
2024-10-15 21:48:28 +0530

    First Commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,9 @@ function Login(){
     return (
       <section className="row">
         <section className="col-3 mx-auto mt-5">
-            <h4 className="text-center">amazon.in</h4>
+        <div className="image mx-auto">
+        <img src="png.png" className="logo" ></img>
+        </div>
            <section className="border p-4 rounded">
             <h3 className="mb-4">Sign in</h3>
              {/* email */}
@@ -12,8 +14,8 @@ function Login(){
             <label for="exampleFormControlInput1" className="form-label">
               Email address
             </label>
-            <input
-              type="email"
+            <input 
+              type="email" 
               className="form-control"
               id="exampleFormControlInput1"
               placeholder="Enter your email..."
@@ -34,9 +36,16 @@ function Login(){
           </div>
           {/* password -end */}
           <button className="btn btn-warning w-100 mb-3 mt-2">Login</button>
-          <p className="text-small">
-          By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
-          </p>
+
+        <p className="text-small">
+            By continuing, you agree to Amazon's <a href="#">Conditions of Use </a> and <a href="#">Privacy Notice</a>.
+        </p>
+
+        <a href="#" className="under">Need help?</a>
+        <hr/>
+        <p> Buying for work?</p>
+        <a href="#" className="under">Shop on Amazon Business</a>
+      
            </section>
         </section>
       </section>
